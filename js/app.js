75bb88a331190b7cc6fb83a21dc9751ddcedae44
Peter O'Shaughnessy <peter.oshaughnessy@pebblecode.com>
2015-09-03 14:04:30 +0100

    Add WebVR

--- js/app.js
+++ js/app.js
@@ -7,7 +7,11 @@
     ambientLight,
     spotLight,
     objLoader = new THREE.OBJLoader(),
-    objMtlLoader = new THREE.OBJMTLLoader();
+    objMtlLoader = new THREE.OBJMTLLoader(),
+    vrEffect,
+    controls,
+    orbitControls,
+    webVRManager;
 
   function init() {
 
@@ -44,13 +48,88 @@
 
     initLights();
 
-    ambientLight = new THREE.AmbientLight( 0x404040 );
-    scene.add( ambientLight );
+    initVR();
+
+    initControls();
 
     // Debug axes
     //initDebugAxes(100);
 
     window.addEventListener('resize', onResize, false);
+
+  }
+
+
+  function initModels() {
+
+    loadDrum('red', [-200, -70, 30]);
+    loadDrum('blue', [-70, -50, 0]);
+    loadDrum('red', [50, -50, 30]);
+
+    loadHiHat([250, -10, 30]);
+
+  }
+
+  function initLights() {
+
+    ambientLight = new THREE.AmbientLight( 0x404040 );
+    scene.add( ambientLight );
+
+    spotLight = new THREE.SpotLight( 0xFFFFFF, 2.0 );
+
+    spotLight.castShadow = true;
+    //spotLight.onlyShadow = true;
+    spotLight.shadowCameraNear = camera.near;
+
+    spotLight.position.set( 0, 500, 700);
+    spotLight.target.position.set( 0, 0, -100 );
+    //spotLight.shadowCameraVisible = true;
+
+    scene.add( spotLight );
+    scene.add( spotLight.target );
+
+  }
+
+  function initDebugAxes(axisLength) {
+
+    // Shorthand function
+    function v(x, y, z) {
+      return new THREE.Vector3(x, y, z);
+    }
+
+    // Create axis (point1, point2, colour)
+    function createAxis(p1, p2, color) {
+      var line, lineGeometry = new THREE.Geometry(),
+        lineMat = new THREE.LineBasicMaterial({color: color, lineWidth: 1});
+      lineGeometry.vertices.push(p1, p2);
+      line = new THREE.Line(lineGeometry, lineMat);
+      scene.add(line);
+    }
+
+    createAxis(v(-axisLength, 0, 0), v(axisLength, 0, 0), 0xFF0000);
+    createAxis(v(0, -axisLength, 0), v(0, axisLength, 0), 0x00FF00);
+    createAxis(v(0, 0, -axisLength), v(0, 0, axisLength), 0x0000FF);
+
+  }
+
+  function initVR() {
+
+    vrEffect = new THREE.VREffect(renderer);
+
+    // Initialize the WebVR manager.
+    webVRManager = new WebVRManager(renderer, vrEffect, {
+      hideButton: false
+    });
+
+    window.addEventListener('message', handlePostMessage);
+
+  }
+
+  function initControls() {
+
+    controls = new THREE.VRControls(camera);
+    orbitControls = new THREE.OrbitControls(camera);
+    orbitControls.noZoom = true;
 
   }
 
@@ -135,13 +214,6 @@
 
           count++;
         }
-
-        //if ( child instanceof THREE.Mesh ) {
-        //
-        //  child.material = hiHatMaterialSilver;
-        //
-        //}
-
       });
 
       object.scale.set(30, 30, 30);
@@ -153,67 +225,31 @@
 
   }
 
-  function initModels() {
-
-    loadDrum('red', [-200, -70, 30]);
-    loadDrum('blue', [-70, -50, 0]);
-    loadDrum('red', [50, -50, 30]);
-
-    loadHiHat([250, -10, 30]);
-
-  }
-
-  function initLights() {
-
-    spotLight = new THREE.SpotLight( 0xFFFFFF, 2.0 );
-
-    spotLight.castShadow = true;
-    //spotLight.onlyShadow = true;
-    spotLight.shadowCameraNear = camera.near;
-
-    spotLight.position.set( 0, 500, 700);
-    spotLight.target.position.set( 0, 0, -100 );
-    //spotLight.shadowCameraVisible = true;
-
-    scene.add( spotLight );
-    scene.add( spotLight.target );
-
-  }
-
-  function initDebugAxes(axisLength) {
-
-    // Shorthand function
-    function v(x, y, z) {
-      return new THREE.Vector3(x, y, z);
-    }
-
-    // Create axis (point1, point2, colour)
-    function createAxis(p1, p2, color) {
-      var line, lineGeometry = new THREE.Geometry(),
-        lineMat = new THREE.LineBasicMaterial({color: color, lineWidth: 1});
-      lineGeometry.vertices.push(p1, p2);
-      line = new THREE.Line(lineGeometry, lineMat);
-      scene.add(line);
-    }
-
-    createAxis(v(-axisLength, 0, 0), v(axisLength, 0, 0), 0xFF0000);
-    createAxis(v(0, -axisLength, 0), v(0, axisLength, 0), 0x00FF00);
-    createAxis(v(0, 0, -axisLength), v(0, 0, axisLength), 0x0000FF);
-  }
-
   function onResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
   }
 
+  function handlePostMessage(e) {
+    if (e.data.mode == 'vr') {
+      manager.enterVR();
+    }
+    if (e.data.mode == 'mono') {
+      manager.exitVR();
+    }
+  }
+
   function animate() {
 
     //console.log('animate');
 
-    renderer.render(scene, camera);
+    //renderer.render(scene, camera);
 
-    camera.lookAt(new THREE.Vector3(0,0,0));
+    //camera.lookAt(new THREE.Vector3(0,0,0));
+
+    controls.update();
+    vrEffect.render(scene, camera);
 
     requestAnimationFrame(animate);
 
